1ebf78b061bd6ea46d2fa32bf259aed7c15bea50
hmtai6 <hmtai6@student.cityu.edu.hk>
2015-04-15 11:15:01 +0300

    Create AnalogReadSerial.ino

---
+++ AnalogReadSerial.ino
@@ -0,0 +1,50 @@
+/*
+  AnalogReadSerial
+  Reads an analog input on pin 0, prints the result to the serial monitor.
+  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
+
+ This example code is in the public domain.
+ */
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  
+  //set port to output
+  for(int  i = 0 ; i < 13; i++){
+    pinMode(i, OUTPUT); 
+  }
+  for(int i = 24; i< 28; i++){
+    pinMode(i, OUTPUT); 
+  }
+  
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  while (Serial.available() > 0) {
+    //0100000000000000
+    //check input from computer  
+    for(int  i = 0 ; i < 13; i++){
+      int result = Serial.read();
+      digitalWrite(i, ( result== 49 )?HIGH:LOW );
+    }
+    for(int i = 24; i< 28; i++){
+      int result = Serial.read();
+      digitalWrite(i, (result == 49 )?HIGH:LOW );
+    }
+    
+  }
+  //read data
+  analogReadResolution(14);
+  for(int i = 0 ; i < 12; i++){
+    String portWillRead = "A"+String(i);
+    // read the input on analog pin 0:
+    int sensorValue = analogRead(i);
+    // print out the value you read:
+    Serial.print( String(sensorValue) + "\t");
+  }
+  Serial.println();
+  delay(10);        // delay in between reads for stability
+}
